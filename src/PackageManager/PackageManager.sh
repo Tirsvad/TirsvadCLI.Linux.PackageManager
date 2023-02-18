41d5404cf0c92ce0187d63a3348e28b05003928c
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2023-02-18 23:11:46 +0100

    Fixed Namespaces

--- src/PackageManager/PackageManager.sh
+++ src/PackageManager/PackageManager.sh
@@ -3,7 +3,7 @@
 TCLI_PACKAGEMANAGER_PATH_ROOT="$(dirname "$(realpath "${BASH_SOURCE}")")"
 TCLI_PACKMANAGER_REMOTE_SERVER=0
 TCLI_PACKMANAGER_REMOTE_SERVER_IP=0
-TCLI_PACKMANAGER_REMOTE_SERVER_PORT=0
+TCLI_PACKMANAGER_REMOTE_SERVER_PORT=22
 
 case $DISTRIBUTION_ID in
 "Debian GNU/Linux")
@@ -14,26 +14,17 @@ case $DISTRIBUTION_ID in
 	;;
 esac
 
-
 tcli_packageManager_install() {
 	if [ $? -ne 0 ]; then
 		infoscreenwarn
 		printf "${FUNCNAME[0]}: WARNING\n"
 		return 1
 	fi
-	case $DISTRIBUTION_ID in
-	"Debian GNU/Linux")
-		local cmd="DEBIAN_FRONTEND=noninteractive apt-get -qq install"
-		;;
-	"Ubuntu")
-		local cmd="DEBIAN_FRONTEND=noninteractive apt-get -qq install"
-		;;
-	esac
 	if (( $TCLI_PACKMANAGER_REMOTE_SERVER == 0 )); then
-		$cmd $@
+		$TCLI_PKGS_INSTALL_CMD $@
 	else
-		echo "ssh -p $TCLI_PACKMANAGER_REMOTE_SERVER_PORT root@$TCLI_PACKMANAGER_REMOTE_SERVER_IP $cmd $@"
-		ssh -p $TCLI_PACKMANAGER_REMOTE_SERVER_PORT root@$TCLI_PACKMANAGER_REMOTE_SERVER_IP $cmd $@
+		echo "ssh -p $TCLI_PACKMANAGER_REMOTE_SERVER_PORT root@$TCLI_PACKMANAGER_REMOTE_SERVER_IP $TCLI_PKGS_INSTALL_CMD $@"
+		ssh -p $TCLI_PACKMANAGER_REMOTE_SERVER_PORT root@$TCLI_PACKMANAGER_REMOTE_SERVER_IP $TCLI_PKGS_INSTALL_CMD $@
 	fi
 }
 
