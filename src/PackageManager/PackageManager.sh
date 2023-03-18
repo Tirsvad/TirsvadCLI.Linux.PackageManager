15790eb1aa42730e2c8df22b82f69191182b7198
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2023-03-18 16:01:40 +0100

    Better error info

--- src/PackageManager/PackageManager.sh
+++ src/PackageManager/PackageManager.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-TCLI_PACKAGEMANAGER_PATH_ROOT="$(dirname "$(realpath "${BASH_SOURCE}")")"
-TCLI_PACKMANAGER_REMOTE_SERVER=0
-TCLI_PACKMANAGER_REMOTE_SERVER_IP=0
-TCLI_PACKMANAGER_REMOTE_SERVER_PORT=22
+declare TCLI_PACKAGEMANAGER_PATH_ROOT="$(dirname "$(realpath "${BASH_SOURCE}")")"
+declare TCLI_PACKAGEMANAGER_REMOTE_SERVER=0
+declare TCLI_PACKAGEMANAGER_REMOTE_SERVER_IP=0
+declare TCLI_PACKAGEMANAGER_REMOTE_SERVER_PORT=22
+declare TCLI_PACKAGEMANAGER_TERMINAL_OUTPUT
 
-case $DISTRIBUTION_ID in
+case $TCLI_LINUX_DISTRIBUTION_ID in
 "Debian GNU/Linux")
 	. $TCLI_PACKAGEMANAGER_PATH_ROOT/inc/packagesDebian10.sh
 	;;
@@ -15,32 +16,42 @@ case $DISTRIBUTION_ID in
 esac
 
 tcli_packageManager_install() {
-	if [ $? -ne 0 ]; then
+	local _cmd
+	if (( $TCLI_PACKAGEMANAGER_REMOTE_SERVER == 0 )); then
+		_cmd=($TCLI_PKGS_INSTALL_CMD $@)
+	else
+		# _cmd=("ssh -p $TCLI_PACKAGEMANAGER_REMOTE_SERVER_PORT root@$TCLI_PACKAGEMANAGER_REMOTE_SERVER_IP $TCLI_PKGS_INSTALL_CMD ")
+		_cmd=(ssh)
+		_cmd+=(-p $TCLI_PACKAGEMANAGER_REMOTE_SERVER_PORT)
+		_cmd+=(root@$TCLI_PACKAGEMANAGER_REMOTE_SERVER_IP)
+		_cmd+=($TCLI_PKGS_INSTALL_CMD $@)
+	fi
+	TCLI_PACKAGEMANAGER_TERMINAL_OUTPUT=$(${_cmd[@]})
+	if [ ! $? -eq 0 ]; then
 		infoscreenwarn
-		printf "${FUNCNAME[0]}: WARNING\n"
+		printf "Tried to install \"$@\" but failed: $_cmd\n"
 		return 1
 	fi
-	if (( $TCLI_PACKMANAGER_REMOTE_SERVER == 0 )); then
-		$TCLI_PKGS_INSTALL_CMD $@
-	else
-		echo "ssh -p $TCLI_PACKMANAGER_REMOTE_SERVER_PORT root@$TCLI_PACKMANAGER_REMOTE_SERVER_IP $TCLI_PKGS_INSTALL_CMD $@"
-		ssh -p $TCLI_PACKMANAGER_REMOTE_SERVER_PORT root@$TCLI_PACKMANAGER_REMOTE_SERVER_IP $TCLI_PKGS_INSTALL_CMD $@
-	fi
+	while(( "$#" ))
+	do
+		printf "Installed $1\n"
+		shift
+	done
 }
 
 tcli_packageManager_system_update() {
-	case $DISTRIBUTION_ID in
+	case $TCLI_LINUX_DISTRIBUTION_ID in
 	"Debian GNU/Linux")
-		local cmd="DEBIAN_FRONTEND=noninteractive apt-get update"
+		local cmd="DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null"
 		;;
 	"Ubuntu")
-		local cmd="DEBIAN_FRONTEND=noninteractive apt-get update"
+		local cmd="DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null"
 		;;
 	esac
-	if (( $TCLI_PACKMANAGER_REMOTE_SERVER == 0 )); then
+	if (( $TCLI_PACKAGEMANAGER_REMOTE_SERVER == 0 )); then
 		$cmd
 	else
-		ssh -p $TCLI_PACKMANAGER_REMOTE_SERVER_PORT root@$TCLI_PACKMANAGER_REMOTE_SERVER_IP $cmd
+		ssh -p $TCLI_PACKAGEMANAGER_REMOTE_SERVER_PORT root@$TCLI_PACKAGEMANAGER_REMOTE_SERVER_IP $cmd
 	fi
 	if [ $? -ne 0 ]; then
 		infoscreenwarn
@@ -49,18 +60,18 @@ tcli_packageManager_system_update() {
 }
 
 tcli_packageManager_system_upgrade() {
-	case $DISTRIBUTION_ID in
+	case $TCLI_LINUX_DISTRIBUTION_ID in
 	"Debian GNU/Linux")
-		local cmd="DEBIAN_FRONTEND=noninteractive apt-get -y upgrade"
+		local cmd="DEBIAN_FRONTEND=noninteractive apt-get -y upgrade > /dev/null"
 		;;
 	"Ubuntu")
-		local cmd="DEBIAN_FRONTEND=noninteractive apt-get -y upgrade"
+		local cmd="DEBIAN_FRONTEND=noninteractive apt-get -y upgrade > /dev/null"
 		;;
 	esac
-	if (( $TCLI_PACKMANAGER_REMOTE_SERVER == 0 )); then
+	if (( $TCLI_PACKAGEMANAGER_REMOTE_SERVER == 0 )); then
 		$cmd
 	else
-		ssh -p $TCLI_PACKMANAGER_REMOTE_SERVER_PORT root@$TCLI_PACKMANAGER_REMOTE_SERVER_IP $cmd
+		ssh -p $TCLI_PACKAGEMANAGER_REMOTE_SERVER_PORT root@$TCLI_PACKAGEMANAGER_REMOTE_SERVER_IP $cmd
 	fi
 	if [ $? -ne 0 ]; then
 		infoscreenwarn
